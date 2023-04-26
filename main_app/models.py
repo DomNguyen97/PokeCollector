e897fbbdbfefe875d6812537883da777f918ea5c
SeksiBoiDom <ndominic56@gmail.com>
2023-04-26 10:24:20 -0400

    add moves for pokemon

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.urls import reverse
+
+ELEMENTS = (
+    ('F', 'Fire'),
+    ('W', 'Water'),
+    ('L', 'Lighting')
+)
 
 # Create your models here.
 class Pokemon(models.Model):
@@ -6,6 +13,24 @@ class Pokemon(models.Model):
   type = models.CharField(max_length=100)
   abilities = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
-  
+
   def __str__(self):
-    return self.name
+    return f'{self.name} ({self.id})'
+
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'pokemon_id': self.id})  
+
+class Move(models.Model):
+  name = models.CharField(max_length=50)
+  element = models.CharField(
+    max_length=1,
+      choices=ELEMENTS,
+      default=ELEMENTS[0][0]
+  )
+  pokemon = models.ForeignKey(
+    Pokemon, 
+    on_delete=models.CASCADE)
+
+  def __str__(self): 
+    return f"{self.get_element_display()} on {self.name}"
+  
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Pokemon
+from .forms import MoveForm
+
 
 
 # Create your views here.
@@ -17,6 +20,30 @@ def pokemons_index(request):
 
 def pokemons_detail(request, pokemon_id):
   pokemon = Pokemon.objects.get(id=pokemon_id)
-  return render(request, 'pokemons/detail.html', { 
-    'pokemon': pokemon 
+  # instantiate FeedingForm to be rendered in the template
+  move_form = MoveForm()
+  return render(request, 'pokemons/detail.html', {
+    # include the cat and feeding_form in the context
+    'pokemon': pokemon, 'move_form': move_form
   })
+
+class PokemonCreate(CreateView):
+  model = Pokemon
+  fields = '__all__'
+
+class PokemonUpdate(UpdateView):
+  model = Pokemon
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['type','abilities','description']
+
+class PokemonDelete(DeleteView):
+  model = Pokemon
+  success_url = '/pokemons'  
+
+def add_move(request, pokemon_id):
+  form = MoveForm(request.POST)
+  if form.is_valid():
+    new_move = form.save(commit=False)
+    new_move.pokemon_id = pokemon_id
+    new_move.save()
+    return redirect('detail', pokemon_id=pokemon_id)
